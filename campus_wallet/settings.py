a879ccbb6e8f2c76f4e321bd5ef39d6e0ea66d02
joel joby <jj9821@srmist.edu.in>
2025-11-20 02:51:04 +0530

    Add PostgreSQL DB config

--- campus_wallet/settings.py
+++ campus_wallet/settings.py
@@ -73,11 +73,9 @@ WSGI_APPLICATION = 'campus_wallet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(env='DATABASE_URL', default='sqlite:///db.sqlite3')
 }
 
 
